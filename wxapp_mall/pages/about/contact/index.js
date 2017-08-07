fc8672faff800fa6c96d019e8c27a09a5e2a8155
ChenZhaoYu <790348264@qq.com>
2017-08-07 09:29:13 +0800

    新增

--- wxapp_mall/pages/about/contact/index.js
+++ wxapp_mall/pages/about/contact/index.js
@@ -1,66 +1,34 @@
-// pages/about/contact/index.js
+// pages/contact/contact.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
+    companyInfo: {
+      person: '陈先生',
+      tell: '020-00000000',
+      mobile: '13169579266',
+      email: '790348264@qq.com',
+      address: '深圳市南山区深圳湾生态科技园5栋C座10F'
+    },
+    lon: 113.952510,
+    lat: 22.527310,
+    markers: [{
+      id: 0,
+      iconPath: '/static/mark.png',
+      latitude: 22.527310,
+      longitude: 113.952510,
+      width: 48,
+      height: 48,
+      callout: {
+        content: '深圳市天助人和信息技术有限公司',
+        borderRadius: 3,
+        padding: 4,
+        boxShadow: '0 0 12px rgba(0,0,0,.2)'
+      }
+    }]
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  makePhone: function (e) {
+    let nun = e.currentTarget.dataset.tell;
+    wx.makePhoneCall({
+      phoneNumber: nun
+    })
   }
 })
